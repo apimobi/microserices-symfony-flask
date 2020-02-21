062a975fc829d192944b1570e06690c3e396a3f1
Vincent Montuclard <vm@MacBook-Pro-de-Vincent.local>
2020-02-21 21:33:03 +0100

    flask environment

--- flask/app.py
+++ flask/app.py
@@ -2,10 +2,11 @@ from flask import Flask
 import requests
 import json
 from markupsafe import escape
+from os import environ
 
 
-geo_base_url = "https://geo.ipify.org/api/v1?apiKey="
-geo_api_key = ""
+GEO_BASE_URL = "https://geo.ipify.org/api/v1?apiKey="
+GEO_API_KEY = environ.get('GEO_API_KEY')
 
 app = Flask(__name__)
 
@@ -15,7 +16,13 @@ def hello():
 
 @app.route('/by-ip/<ip>')
 def byIp(ip):
-    r = requests.get(geo_base_url+geo_api_key+"&ipAddress={}".format(escape(ip)))
+    r = requests.get(GEO_BASE_URL+GEO_API_KEY+"&ipAddress={}".format(escape(ip)))
+    data = json.loads(r.text)
+    return data
+
+@app.route('/by-address/<address>')
+def byAddress(address):
+    r = requests.get(GEO_BASE_URL+GEO_API_KEY+"&ipAddress={}".format(escape(address)))
     data = json.loads(r.text)
     return data
 
